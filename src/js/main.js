02e1c104ed16b22d16f287860c8501bdfaf80cf5
FxH24 <farouk.hamdaoui@student.ehb.be>
2026-05-16 15:45:25 +0200

    Update main.js
    toevoegen van de fetch-code zodat data kan worden opgehaald van API en  deze toont in de console

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,23 @@
 console.log("Hogwarts Character Explorer")
 //dit is gewoon om te testen of het script laad
+
+const API_URL= "https://api.potterdb.com/v1/characters?page[size]=100";
+
+const getCharacters= async ()=> {
+  try{ // zorgt ervoor da
+    const response = await fetch (API_URL);
+    
+    if(! response.ok){ 
+      throw new Error(`HTTP error:${response.status}`);
+    }
+    const json= await response.json();
+    console.log("Full response:",json);
+    console.log("Characters array:", json.data.length);
+  }catch(error){
+    console.log("Something went wrong", error.message);
+  }
+
+
+};
+getCharacters()
+
